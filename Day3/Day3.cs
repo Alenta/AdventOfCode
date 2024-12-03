dc0711a68a5cd3df027857d3d99c37ef47cef4fd
Alenta <martin.fossen@gmail.com>
2024-12-03 11:15:17 +0100

    Day3 P2 Solved

--- Day3/Day3.cs
+++ Day3/Day3.cs
@@ -1,81 +1,62 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 
 class Day3 {
 
     static void Main(string[] args) {
-        List<int> totalTall = [];
-
+        List<int> totalNumbers = [];
         try
         {
-
             string? line;        
             // Regex pattern to match 'mul(x,y)' where x and y are whole numbers
-            string pattern = @"mul\((\d+),(\d+)\)";
-                   
-            // Match the first occurrence
+            string mulPattern = @"mul\((\d+),(\d+)\)";
             using StreamReader sr = new StreamReader("input.txt");
-            int totalValue = 0;
-            
+            // Matching starts off true
+            bool doMatch = true;
+
             while ((line = sr.ReadLine()) != null) {
                 bool lineDone = false;
                 int substrIndex = 0;
 
-                while (!lineDone)
-                {
-                    Match match = Regex.Match(line.Substring(substrIndex), pattern);
+                while (!lineDone) {
+                    Match match = Regex.Match(line.Substring(substrIndex), mulPattern);
 
-                    if (match.Success)
-                    {
+                    if (match.Success) {
+                        if (line.Count() > substrIndex + match.Index) {
+
+                            int length = match.Index;
+                            string substring = line.Substring(substrIndex, length);
+                            if (substring.Contains("do()")) {
+                                Console.WriteLine("We're matching again!");
+                                doMatch = true;
+                            }
+                            if (substring.Contains("don't()")) {
+                                Console.WriteLine("No more matching plz :c");
+                                doMatch = false;
+                            }
+                        }
+
                         int x = int.Parse(match.Groups[1].Value);
                         int y = int.Parse(match.Groups[2].Value);
-                        totalTall.Add(x * y); // Example operation
+                        if(doMatch) totalNumbers.Add(x * y);
 
                         substrIndex = substrIndex + match.Index + match.Length;
                     }
-                    else
-                    {
+                    else {
                         Console.WriteLine("No more matches on this line, line done");
                         lineDone = true;
                     }
-                }
-
-                //Console.WriteLine("Total calculations: " + string.Join(", ", totalTall));
-                
+                }                
             }
         }
-        catch (System.Exception)
+        catch (System.Exception e)
         {
-            throw;
+            Console.WriteLine("Exception: " + e.Message);
         }                  
         finally{
             int total = 0;
-            foreach (int nr in totalTall)
-            {
-                total += nr;
-            }
+            foreach (int nr in totalNumbers) total += nr;
             Console.WriteLine(total + " THIS IS MY NUMBER");
         }
     }
-
-    public static int MultNumbers(int a, int b){
-        return a*b;
-    }
-
-
-
-    public static int ParseInt(string stringToParse)
-    {
-        try
-        {
-            int number = Int32.Parse(stringToParse);
-            return number;
-        }
-        catch (FormatException)
-        {
-            Console.WriteLine($"Cannot parse {stringToParse} to a pure number");
-            return 0;
-        }
-    }
-    
-
 }
